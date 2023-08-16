d751d956ce487ccb63b123c60366486c6dfb7e77
sweetsoph <sophia.verardods@gmail.com>
2023-08-16 17:03:20 -0300

    starting project, header component undone

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import Header from "./components/header/header";
+
+function Home() {
+  return (
+    <div className="main">
+      <Header/>
+      <h1>Teste</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/header/header.js
@@ -0,0 +1,18 @@
+import "./header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div></div>
+      <nav>
+        <ul>
+          <li><a>Home</a></li>
+          <li><a>Projetos</a></li>
+          <li><a>Empresa</a></li>
+          <li><a>Contato</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+export default Header;
